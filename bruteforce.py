5d8a5fd4acc766caf81062623623c60f2460ef7c
Ace-CoLd-HOoDy <121887597+Ace-CoLd-HOoDy@users.noreply.github.com>
2025-01-27 15:08:58 +0200

    52

--- bruteforce.py
+++ bruteforce.py
@@ -1,1 +1,21 @@
-print("test")
+def check_password(password):
+    correct_password = "Academy24"
+    if password == correct_password:
+        return True
+    return False
+
+def brute_force(password_list):
+    for password in password_list:
+        print(checking password {password})
+        if check_password(password):
+            print(access granted {password})
+            return password
+        print(access denied)
+        return None
+
+with open('passwords.txt', 'r') as file:
+    passwords = file.readlines()
+ 
+passwords = [password.strip()for password in passwords]
+
+brute_force(passwords)
